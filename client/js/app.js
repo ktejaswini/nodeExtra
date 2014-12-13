032c6c0765e10c478db2a1dc5cc3cbd482ca34e6
SadhanaP <sadhana.106@gmail.com>
2014-12-12 19:53:21 -0800

    Update app.js

--- client/js/app.js
+++ client/js/app.js
@@ -11,18 +11,7 @@ gumballApp.config(function ($routeProvider, $locationProvider) {
       when('/index', {
         templateUrl: 'views/entry.html',
       }).
-      when('/edit', {
-        templateUrl: 'views/editMachines.html',
-        controller: 'editCtrl'
-      }).
-      when('/edit/:id', {
-        templateUrl: 'views/editMachines.html',
-        controller: 'delCtrl'
-      }).
-      when('/add', {
-        templateUrl: 'views/editMachines.html',
-        controller: 'addCtrl'
-      }).
+     
       when('/select/:id', {
         templateUrl: 'views/main.html',
         controller: 'mainCtrl'
